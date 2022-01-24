cb5e893867eac7ea1482d547ee70ed29a637a763
kamyshenJava <kamyshen@tutanota.com>
2022-01-24 12:04:37 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Ошибка: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Примечание: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 20, "ошибка ввода/вывода");
+        Error error3 = new Error(true, 100, "ошибка чтения данных");
+        Error error4 = new Error(true, 1, "ошибка в коде");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+        error4.printInfo();
+    }
+}

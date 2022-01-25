c1930808ac706fbf2d83cc4f5ef5c892fb0a8663
kamyshenJava <kamyshen@tutanota.com>
2022-01-25 17:36:45 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,21 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null && item.getName().equals(key)) {
+                rsl[size++] = item;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
     }
 }

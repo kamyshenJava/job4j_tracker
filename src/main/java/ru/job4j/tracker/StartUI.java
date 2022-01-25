82d2ea075451bdbabecabd785505ea080a835d50
kamyshenJava <kamyshen@tutanota.com>
2022-01-25 20:51:40 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,6 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item current = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(current.getCreated().format(formatter));
+        System.out.println(current.toString());
     }
 }

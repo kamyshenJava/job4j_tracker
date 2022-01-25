637d691c5e3084ac2d682cecc9878b4af68d4df4
kamyshenJava <kamyshen@tutanota.com>
2022-01-25 08:15:49 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587 #254895]. Добавил тестов.

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -8,6 +8,14 @@ public class PointTest {
 
     @Test
     public void distance() {
+        Point a = new Point(10, -6);
+        Point b = new Point(-14, 6);
+        double rsl = a.distance(b);
+        assertThat(rsl, closeTo(26.833, 0.01));
+    }
+
+    @Test
+    public void distance1() {
         Point a = new Point(0, 0);
         Point b = new Point(5, 6);
         double rsl = a.distance(b);

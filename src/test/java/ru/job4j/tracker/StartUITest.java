c19a646d488695749eed0cff4413f0a3af6e9d94
kamyshenJava <kamyshen@tutanota.com>
2022-01-29 12:16:51 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -226,4 +226,26 @@ public class StartUITest {
                         + "1. Exit Program" + ln
         ));
     }
+
+    @Test
+    public void whenInvalidThenExitOutputIsSuccessful() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"7", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                        "Menu:" + ln
+                                + "0. Exit Program" + ln
+                                + "Wrong input, you can select: 0 .. 0" + ln
+                                + "Menu:" + ln
+                                + "0. Exit Program" + ln
+                )
+        );
+    }
 }

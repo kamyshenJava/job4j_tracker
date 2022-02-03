7483de88d05397edcc1ad263262149e3da7da6f7
kamyshenJava <kamyshen@tutanota.com>
2022-02-03 12:40:08 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #259299]. Рефакторинг

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,7 +8,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            index = task.getPriority() > element.getPriority() ? index + 1 : index;
+             if (task.getPriority() < element.getPriority()) {
+                 break;
+             }
+             index++;
         }
         this.tasks.add(index, task);
     }

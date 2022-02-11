661b74dcae4201a79512d63df4c9a507b0e3eb4b
kamyshenJava <kamyshen@tutanota.com>
2022-02-11 08:23:42 +0300

    2. Аттестация [#49777 #263028]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -46,7 +46,6 @@ public class Analyze {
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream.flatMap(pupil -> pupil.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
